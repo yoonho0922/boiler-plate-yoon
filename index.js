da6799e5ca6a4d595acdef695c43313160b75296
yoonho <yoonho0922@naver.com>
2021-03-20 00:26:00 +0900

    feat : User 스키마  등록 및 몽고디비 사용

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyPaser = require('body-parser')
+const User = require('./models/User')
+
+
+// application/x-www-form-urlencoded 형식의 데이터를 가져오기위한 설정
+app.use(bodyPaser.urlencoded({extended: true}))
+// application/json
+app.use(bodyPaser.json())
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb+srv://yoonho:990922@cluster0.fk6wc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', {
@@ -11,6 +19,19 @@ mongoose.connect('mongodb+srv://yoonho:990922@cluster0.fk6wc.mongodb.net/myFirst
 app.get('/', (req, res) => {
     res.send('Hello World!')
 });
+
+app.post('/register', (req, res) => {
+    
+    // 회원 가입 할때 필요한 정보들을 client에서 가져와 데이터베이스에 넣음
+
+    const user = new User(req.body)
+
+    user.save((err, userInfo) =>{
+        if(err) return res.json({ success: false, err })
+
+        return res.status(200).json({ success: true })
+    })
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
